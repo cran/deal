68ed019bc3bbd34d90de9689b3617ff1d0bf66d3
Claus Dethlefsen <cld@rn.dk>
2007-04-19 00:00:00 +0000

    version 1.2-30

--- R/postc.R
+++ R/postc.R
@@ -2,8 +2,8 @@
 ## Author          : Claus Dethlefsen
 ## Created On      : Tue Mar 12 06:52:02 2002
 ## Last Modified By: Claus Dethlefsen
-## Last Modified On: Thu Jul 24 15:18:29 2003
-## Update Count    : 143
+## Last Modified On: Fri Apr 20 09:25:29 2007
+## Update Count    : 144
 ## Status          : Unknown, Use with caution!
 ###############################################################################
 ##
@@ -83,32 +83,32 @@ post <- function(mu,tau,rho,phi,y,z,timetrace=FALSE) {
     list(mu=mu.n,tau=tau.n,rho=rho.n,phi=phi.n,loglik=loglik)
 }
 
-postM <- function(mu,tau,rho,phi,y,z,timetrace=FALSE) {
-    ## Posterior for continuous node with continuous parents
-    ## written as Matrix notation in R (needs Matrix)
-    if (timetrace) {t1 <- proc.time();cat("[postM ")}
+## postM <- function(mu,tau,rho,phi,y,z,timetrace=FALSE) {
+##     ## Posterior for continuous node with continuous parents
+##     ## written as Matrix notation in R (needs Matrix)
+##     if (timetrace) {t1 <- proc.time();cat("[postM ")}
     
-    z <- as.Matrix(z)
-    mu.n  <- solve(as.Matrix(tau+t(z)%*%z))%*%(tau%*%mu+t(z)%*%y)
-    tau.n <- tau + t(z)%*%z
-    rho.n <- rho + length(y)
-    phi.n <- phi + t(y - z%*%mu.n)%*%y + t(mu - mu.n)%*%tau%*%mu
+##     z <- as.Matrix(z)
+##     mu.n  <- solve(as.Matrix(tau+t(z)%*%z))%*%(tau%*%mu+t(z)%*%y)
+##     tau.n <- tau + t(z)%*%z
+##     rho.n <- rho + length(y)
+##     phi.n <- phi + t(y - z%*%mu.n)%*%y + t(mu - mu.n)%*%tau%*%mu
 
 
-    loglik <- 0
-    s <- as.numeric(phi)/rho*(diag(nrow(z))+ z%*%solve(tau)%*%t(z))
-    k <- lgamma( (rho + length(y))/2 ) - lgamma(rho/2)-0.5*log(det(rho*s*pi))
-    ind <- log( 1 + (mahalanobis(y,center=z%*%mu,cov=s,inverted=FALSE))/rho)
-    loglik <- as.numeric(k) - (rho+length(y))/2 * ind
+##     loglik <- 0
+##     s <- as.numeric(phi)/rho*(diag(nrow(z))+ z%*%solve(tau)%*%t(z))
+##     k <- lgamma( (rho + length(y))/2 ) - lgamma(rho/2)-0.5*log(det(rho*s*pi))
+##     ind <- log( 1 + (mahalanobis(y,center=z%*%mu,cov=s,inverted=FALSE))/rho)
+##     loglik <- as.numeric(k) - (rho+length(y))/2 * ind
 
         
-    if (timetrace) {
-        t2 <- proc.time()
-        cat((t2-t1)[1],"]")
-    }
+##     if (timetrace) {
+##         t2 <- proc.time()
+##         cat((t2-t1)[1],"]")
+##     }
 
-    list(mu=mu.n,tau=tau.n,rho=rho.n,phi=phi.n,loglik=loglik)
-}
+##     list(mu=mu.n,tau=tau.n,rho=rho.n,phi=phi.n,loglik=loglik)
+## }
 
 
 postcc <- function(mu,tau,rho,phi,y,z,timetrace=FALSE) {
